6e29f772e8685edb277bcc828c49f9a208a45315
Aydan Ahmadzada <aydanahmadzada1@gmail.com>
2026-03-14 18:03:07 +0400

    Update student_main.c

--- student_main.c
+++ student_main.c
@@ -25,9 +25,28 @@ int main() {
          3. Store the common numbers in the 'intersection' array.
     */
     //----------------------------------------------------------------//
-    {
+    for(i = 0; i < n1; i++) {
+        int flag = 0;
+        for(j = 0; j < n2; j++) {
+            if(set1[i] == set2[j]) {
+                flag = 1;
+                for(int k = 0; k<count; k++){
+                    if (intersection[k] == set1[i]){
+                        flag = 0;
+                        break;
+                    }
+                }
+                break;
 
+            }
+        }
+        if (flag == 1){
+            intersection[count] = set1[i];
+            count++;
+            
+        }
     }
+    
     //----------------------------------------------------------------//
     for(i = 0; i < count; i++) {
         printf("%d ", intersection[i]);
